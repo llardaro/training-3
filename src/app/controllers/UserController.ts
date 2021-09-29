562b468ec29e1c4a99833a1876b2ff7d3470d29b
Leandro Lardaro <leandro.lardaro@gmail.com>
2021-09-29 09:00:33 -0300

    linting

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -2,14 +2,14 @@ const userGet = (req: any, res: any) => {
     return res.json({
         message:
         "get users",
-    });
-};
+    })
+}
 
 const userList = (req: any, res: any) => {
     return res.json({
         message:
         "list users",
-    });
-};
+    })
+}
 
 export default {userGet, userList}
