df1b3b6e30ebfa82c6674561ad15f735f9080e60
Leandro Lardaro <leandro.lardaro@gmail.com>
2021-09-29 13:59:24 -0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express from "express"
-import dotenv from "dotenv"
-import routes from "./routes"
+import express from 'express'
+import dotenv from 'dotenv'
+import routes from './routes'
 
 dotenv.config()
 
 const app = express()
 const port = process.env.SERVER_PORT
 
-app.use("/api/v1/", routes)
+app.use('/api/v1/', routes)
 
 app.listen(port, () => {
-    // tslint:disable-next-line
-    console.log(`${port}`)
+  // tslint:disable-next-line
+  console.log(`${port}`)
 })
