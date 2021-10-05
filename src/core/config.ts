de8794e6db1cb025af0d16e3a62295cdb92f4e77
Leandro Lardaro <leandro.lardaro@gmail.com>
2021-10-05 12:10:09 -0300

    core folder and config file

---
+++ src/core/config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const SERVER_PORT: string = process.env.SERVER_PORT
+
+export default {
+  SERVER_PORT
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import express from 'express'
-import dotenv from 'dotenv'
 
+import config from './core/config'
 import routes from './api/routes/router'
-import sentryInit from './middlewares/sentry.middleware'
-
-dotenv.config()
 
 const app = express()
-const port = process.env.SERVER_PORT
-
-sentryInit(app)
+const port = config.SERVER_PORT
 
 app.use('/api/v1/', routes)
 
