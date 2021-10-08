ce29e1ce23d75345e9fb0ba52f94ca5521fda554
Leandro Lardaro <leandro.lardaro@gmail.com>
2021-10-08 10:07:34 -0300

    Unit tests with chai

---
+++ src/tests/chai-example.test.ts
@@ -0,0 +1,10 @@
+const expect = require('chai').expect
+
+describe('Simple math test', () => {
+	it('Sum', () => {
+		expect(1 + 1).to.equal(2)
+	})
+	it('Other test', () => {
+		expect(2 > 0).to.be.true
+	})
+})
